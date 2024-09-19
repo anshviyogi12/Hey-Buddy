c5c4f41c076e6d2cbe1fc504b93e16510e3aad3c
Ansh Viyogi <auni.viyogi@gmail.com>
2024-09-20 02:39:56 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hey Buddy !!</h1>
+      <h3>By Stephan Rodrigues</h3>
+      <h3>Coming Soon</h3>
+    </div>
+  );
+}
+
+export default App;
